3625878691810b7ebbef7f84cccac7e1916ab25a
Prokash Pul <pprokash102@gmail.com>
2022-03-09 23:24:38 +0600

    password unice letter

--- js/app.js
+++ js/app.js
@@ -1,13 +1,26 @@
-const eamil = document.getElementById('email');
+const email = document.getElementById('email');
 const span = document.getElementsByTagName('span');
-eamil.addEventListener('keyup', () => {
+const password = document.getElementById('password');
+email.addEventListener('keyup', () => {
     const regExp = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-    console.log(eamil.value)
-    if (regExp.test(eamil.value)) {
-        span[0].innerText = "Email Is Vlaid"
+    console.log(email.value)
+    if (regExp.test(email.value)) {
+        span[0].innerText = "Email Is Validate"
         span[0].style.color = "green";
     } else {
-        span[0].innerText = "Email Is not Vlaid"
+        span[0].innerText = "Email Is not Validate"
         span[0].style.color = "red";
+    }
+})
+
+// password validate
+password.addEventListener('keyup', () => {
+    const regExpPas = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,16}$/;
+    if (regExpPas.test(password.value)) {
+        span[1].innerText = 'Password Is Validate';
+        span[1].style.color = 'green';
+    } else {
+        span[1].innerText = 'Password Is not Validate (please set 1Upper 1lower 1number 1spacial correcter use up to 8   ';
+        span[1].style.color = 'red';
     }
 })
