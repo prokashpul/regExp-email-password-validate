2f58d204fb7353954f297186288a47177c3e546d
Prokash Pul <pprokash102@gmail.com>
2022-03-09 21:03:47 +0600

    email vlaidity chake

--- js/app.js
+++ js/app.js
@@ -1,7 +1,13 @@
 const eamil = document.getElementById('email');
-eamil.addEventListener('keydown', () => {
-    const regExp = /^([a-zA-Z0-9!&^%$#@!*]){6,13}$/;
+const span = document.getElementsByTagName('span');
+eamil.addEventListener('keyup', () => {
+    const regExp = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    console.log(eamil.value)
     if (regExp.test(eamil.value)) {
-
+        span[0].innerText = "Email Is Vlaid"
+        span[0].style.color = "green";
+    } else {
+        span[0].innerText = "Email Is not Vlaid"
+        span[0].style.color = "red";
     }
 })
